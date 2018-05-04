d35d1d7dfa47b4e5860fd91a23b784db35369a67
Alexander Tolkachev <alexander.tolkachev@itechart-group.com>
2018-05-04 14:22:33 +0300

    Social links and SEO

--- src/html/data/pages.js
+++ src/html/data/pages.js
@@ -2,32 +2,40 @@ const pages = {
     index: {
         url: "./",
         name: "DiyaGroup",
-        title: "DiyaGroup"
+        title: "DiyaGroup",
+        description: `The primary mission of DiyaGroup is to build technology o bridge the gap of access to 
+                    basic health care information for underprivileged population, in order to assist them
+                    to make safer health care decisions.`,
     },
     ourProjects: {
         url: "./our-projects",
         name: "Our Projects",
         title: "Our Projects - DiyaGroup",
+        description: "The OK to Take app allows you keep an up-to-date list of your current medications and allergies and answer basic drug interaction questions",
     },
     howToHelp: {
         url: "./how-to-help",
         name: "How to Help",
         title: "How to Help - DiyaGroup",
+        description: "If you believe in our mission or know someone else who may have the desire, knowledge or expertise, please help by increasing awareness of DiyaGroup.",
     },
     contactUs: {
         url: "./contact-us",
         name: "Contact Us",
         title: "Contact Us - DiyaGroup",
+        description: "We look forward to hearing from you!",
     },
     privacyPolicy: {
         url: "./privacy-policy",
         name: "Privacy Policy",
         title: "Privacy Policy - DiyaGroup",
+        description: "Privacy policy of DiyaGroup Website",
     },
     termsAndConditions: {
         url: "./terms-and-conditions",
         name: "Terms & Conditions",
         title: "Terms & Conditions - DiyaGroup",
+        description: "Terms and conditions of DiyaGroup Website",
     },
 };
 
