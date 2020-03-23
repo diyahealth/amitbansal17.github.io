0e0dec5b107183c8961a1e29b3661bd0fbf4e20d
Alexander Tolkachev <alexander.tolkachev@itechart-group.com>
2020-03-23 20:50:54 +0300

    Update Google recaptcha key

--- src/html/data/keys.js
+++ src/html/data/keys.js
@@ -1,7 +1,7 @@
 const keys = {
     google: {
         map: "AIzaSyBYO5XrKi3ztcfR1S9AoJcjYtu2K_FdDcY",
-        captcha: "6LekklwUAAAAAJzABpyaAJzLJlsoByFXlMDSzhio",
+        captcha: "6Le-a1MUAAAAACpFUE5aZ5k-os4F_d9svImr_x8m",
         analytics: "UA-118012943-1",
     },
 };
