74bf3965c011ce11f6671d3916aa877f46964ca5
mikita-pasled-itechart <mikita.pasled@itechart-group.com>
2020-06-09 21:49:39 +0300

    DIYA-1201. Update home page

---
+++ src/html/data/reviews.js
@@ -0,0 +1,40 @@
+const reviews = [
+    {
+        image: './images/reviews/sangita.jpg',
+        text: 'diyaMD allows doctor to quickly understand and trust patient\'s health information, and give the safest and best medical care',
+        signature: 'Sangeeta Marwaha, MD',
+        title: 'Chair, Dermatology Technology',
+        logo: './images/reviews/logos/kaiser-permanente.svg',
+    }, 
+    {
+        image: './images/reviews/rajesh.png',
+        text: 'myDiya is designed to help us manage immunization compliance and infectious disease outbreaks in the rural communities of HP, India.',
+        signature: 'Rajesh K Sood, MD',
+        title: 'Epidemiology, District Programme Officer',
+        logo:  './images/reviews/logos/national-health-mission.svg',
+        subline: 'National Health Mission, HP, India',
+        compactLogo: true,
+    },
+    {
+        image: './images/reviews/roman.png',
+        text: 'myDiya is a great place to keep the personal health history properly digitized and always available. Its optional QR code feature is perfect for emergency wrist band for all kinds of outdoors activity.',
+        signature: 'Roman Polunin',
+        title: 'Fremont, California',
+    },
+    {
+        image: './images/reviews/david.png',
+        text: 'myDiya has the potential to revolutionize the medical records industry and become the gateway for patient-generated health information to be merged with clinical data in doctor offices and hospitals. myDiya is a digital health records management technology that will expand health information access, reduce medical costs and errors, and save lives worldwide.',
+        signature: 'David Pidwell',
+        title: 'Partner, Alloy Ventures, Palo Alto CA',
+        subline: 'Faculty, Stanford University Graduate School of Business',
+        logo: './images/reviews/logos/stanford-business.jpg',
+    },
+    {
+        image: './images/reviews/katya.jpg',
+        text: 'If you have ever struggled with finding all the docs or remembering all the information related to your health, then you know how painful it is to have a well-organized comprehensive picture of your health. Now you can relax because myDiya will relieve your pain and will secure all your details in one place.',
+        signature: 'Kate Kalento',
+        title: 'Minsk, Belarus',
+    },
+];
+
+module.exports = reviews;
