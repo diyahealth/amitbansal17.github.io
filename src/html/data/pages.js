1930fd670da0ab077cedaf2f202e6b56f6fa3185
mikita-pasled-itechart <mikita.pasled@itechart-group.com>
2020-07-02 20:58:24 +0300

    DIYA-1258. Generate webpack plugin instances for pages from data object

--- src/html/data/pages.js
+++ src/html/data/pages.js
@@ -1,5 +1,7 @@
 const pages = {
     index: {
+        id: "index",
+        fileName: "index",
         url: "./",
         name: "Diya Health",
         title: "Diya Health",
@@ -8,42 +10,42 @@ const pages = {
                     to make safer health care decisions`,
     },
     aboutUs: {
+        fileName: "about-us",
         url: "./about-us",
         name: "About Us",
         title: "About Us - Diya Health",
         description: "Learn about the Diya Health mission and members",
     },
     doctors: {
+        fileName: "doctors",
         url: "./doctors",
         name: "Doctors",
         title: "Doctors - Diya Health",
         description: "The OK to Take app allows you keep an up-to-date list of your current medications and allergies and answer basic drug interaction questions",
     },
     patientsAndFamilies: {
+        fileName: "patients-and-families",
         url: "./patients-and-families",
         name: "Patients & Families",
         title: "Patients & Families - Diya Health",
         description: "",
     },
-    howToHelp: {
-        url: "./how-to-help",
-        name: "How to Help",
-        title: "How to Help - Diya Health",
-        description: "If you believe in our mission or know someone else who may have the desire, knowledge or expertise, please help by increasing awareness of Diya Health",
-    },
     contactUs: {
+        fileName: "contact-us",
         url: "./contact-us",
         name: "Contact Us",
         title: "Contact Us - Diya Health",
         description: "We look forward to hearing from you!",
     },
     privacyPolicy: {
+        fileName: "privacy-policy",
         url: "./privacy-policy",
         name: "Privacy Policy",
         title: "Privacy Policy - Diya Health",
         description: "Privacy policy of Diya Health Website",
     },
     termsAndConditions: {
+        fileName: "terms-and-conditions",
         url: "./terms-and-conditions",
         name: "Terms & Conditions",
         title: "Terms & Conditions - Diya Health",
