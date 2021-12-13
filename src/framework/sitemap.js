354821d4bb5d5b8a7ae5f10da66838f84a32a534
Alexander Tolkachev <alexander.tolkachev@itechart-group.com>
2021-12-13 16:19:32 +0200

    DIYA-1936 Generate sitemap for website

---
+++ src/framework/sitemap.js
@@ -0,0 +1,28 @@
+const static = require("../html/data").pages;
+const fs = require("fs");
+const path = require("path");
+
+const buildStaticUrls = () => Object.values(static).map(x => x.url.substring(2));
+const buildDynamicUrls = (root) => fs.readdirSync(root);
+const buildBlogUrls = (root) => fs.readdirSync(root)
+    .filter(x => path.extname(x) === '.html')
+    .map(x => 'posts/' + x);
+
+function buildSiteMap(dynamicRoot, blogRoot, siteMapOut) {
+    const pageNames = buildStaticUrls()
+        .concat(buildDynamicUrls(dynamicRoot))
+        .concat(buildBlogUrls(blogRoot));
+    const urls = pageNames.map(x => 'https://diyahealth.org/' + encodeURI(x));
+    const content = urls.join('\r\n');
+    const outPath = path.join(siteMapOut, 'sitemap.txt');
+    ensureDirectoryExistence(outPath);
+    fs.writeFileSync(outPath, content, { encoding: 'utf-8' });
+}
+
+function ensureDirectoryExistence(filePath) {
+    const dirname = path.dirname(filePath);
+    fs.mkdirSync(dirname, { recursive: true });
+}
+
+
+module.exports = buildSiteMap;
