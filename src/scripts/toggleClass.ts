46d65d9730cdc3ab2732c4e62cfce8fa69a00bf4
Yauheni Balashka <zhenya@diyahealth.org>
2021-12-30 16:29:42 +0300

    ﻿pricing

--- src/scripts/toggleClass.ts
+++ src/scripts/toggleClass.ts
@@ -1,6 +1,6 @@
-export type ToggleClass = (id: string, classToAdd: string) => void;
+export type ToggleClass = (id: string, classToAdd: string, hash?:boolean) => void;
 
-export function toggleClass(id: string, classToAdd: string) {
+export function toggleClass(id: string, classToAdd: string, hash:boolean=true) {
     if (!classToAdd) {
         return;
     }
@@ -10,6 +10,26 @@ export function toggleClass(id: string, classToAdd: string) {
         return;
     }
 
-    window.location.hash = "#" + id;
+    if (hash) {
+        window.location.hash = "#" + id;
+    }
+
+    
+    element.classList.toggle(classToAdd);
+}
+
+export type toggleClassWithoutHash = (id: string, classToAdd: string) => void;
+
+export function toggleClassWithoutHash(id: string, classToAdd: string) {
+    if (!classToAdd) {
+        return;
+    }
+
+    const element = document.getElementById(id);
+    if (!element) {
+        return;
+    }
+
+    
     element.classList.toggle(classToAdd);
 }
