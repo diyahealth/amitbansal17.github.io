52f07e91d3691680dc29692ffa68f62abab45819
Yauheni Balashka <zhenya@diyahealth.org>
2022-10-21 17:20:56 +0300

    DIYA-67 new page caregiver

---
+++ src/scripts/EstimatedSavings.ts
@@ -0,0 +1,27 @@
+export function estimatedSavings(event, form) {
+    event.preventDefault();
+
+    const physicianCount = +form.elements['physiciansCount'].value;
+    const hourlyWageP = +form.elements['physiciansWage'].value;
+    const nursesCount = +form.elements['nursesCount'].value;
+    const hourlyWageN = +form.elements['nursesWage'].value;
+    const familyCommunication = +form.elements['time'].value;
+    const percentSavings=+form.elements['percent'].value;
+
+const result = (((physicianCount*hourlyWageP*familyCommunication*365) + (nursesCount* hourlyWageN*familyCommunication*365 ))* percentSavings/100) - (50*(physicianCount+nursesCount)*12);
+
+const formatter = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD',
+    minimumFractionDigits: 0, 
+    maximumFractionDigits: 0, 
+  });
+
+(document.getElementById('result-block-value') as HTMLInputElement).innerText =
+  `Projected Annual Cost Savings: ${formatter.format(result)}`;
+
+}
+
+
+
+
