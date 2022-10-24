ea2776964e0a07bb67d58732dcca9f57b2cbf333
Alexander Tolkachev <alexander.tolkachev@itechart-group.com>
2022-10-24 13:18:58 +0100

    DHW-67 Remove outdated pages for hospitals

--- src/html/data/navigation.js
+++ src/html/data/navigation.js
@@ -15,30 +15,6 @@ const buildPage = (title, url, variant, postfix) => {
 
 const pageToLink = (page, variant, postfix) => buildPage(page.name, page.url, variant, postfix);
 
-const buildEmployersDropdown = () => {
-    const titleUrlItems = [
-        { title: 'COVID Compliance-as-a-Service', url: pages.employers.url },
-        { title: 'Wellness Engagement', url: pages.wellness.url },
-    ];
-
-    return titleUrlItems.map(item => buildPage(item.title, item.url, item.variant));
-}
-
-const buildDropdownFromPageWithSubpages = (pageKey, subheader, postfix) => {
-    const page = pages[pageKey];
-    const subpageKeys = page ? Object.keys(pages).filter(subPageKey => pages[subPageKey].parent === pageKey) : undefined;
-
-    if (!subpageKeys || subpageKeys.length === 0) {
-        return undefined;
-    }
-
-    const links = [...subpageKeys.map(pageKey => {
-        const page = pages[pageKey];
-        return page && pageKey !== subheader ? pageToLink(page, null, postfix) : undefined;
-    })].filter(Boolean);
-    return links;
-}
-
 const buildAboutUsDropdown = () => {
     const aboutUsUrl = pages.aboutUs.url;
 
@@ -46,7 +22,6 @@ const buildAboutUsDropdown = () => {
         { title: 'Our Mission', url: `${aboutUsUrl}#mission` },
         { title: 'Our History', url: `${aboutUsUrl}#history` },
         { title: 'Our Team', url: `${aboutUsUrl}#team` },
-       
     ];
 
     return titleUrlItems.map(item => buildPage(item.title, item.url, undefined));
@@ -75,14 +50,12 @@ const buildNavigationLinks = (mode) => {
     navigationLinks.length = 0;
     navigationLinks.push(
         {
-            ...pageToLink(pages.healthSystems, null, postfix),
-            dropdown: buildDropdownFromPageWithSubpages('healthSystems', { name: 'DiyaMD', url: pages.healthSystems.url }, postfix),
-        },
-        {
             ...pageToLink(pages.familyConnect, null, postfix),
             dropdown: buildFamilyDropdown(),
         },
-      
+        {
+            ...pageToLink(pages.healthSystems, null, postfix),
+        },
         {
             ...pageToLink(pages.aboutUs, null, postfix),
             dropdown: buildAboutUsDropdown(postfix),
