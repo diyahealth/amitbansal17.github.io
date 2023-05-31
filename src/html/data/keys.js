cc7ce14a06d915585c3fadb1ce2c06202e1f694f
Alex Tolkachev <alexander.tolkachev@itechart-group.com>
2023-05-31 13:45:00 +0300

    DHW-97 Update Google Analytics Key to support GA4

--- src/html/data/keys.js
+++ src/html/data/keys.js
@@ -2,7 +2,7 @@ const keys = {
     google: {
         map: "AIzaSyBYO5XrKi3ztcfR1S9AoJcjYtu2K_FdDcY",
         captcha: "6Le-a1MUAAAAACpFUE5aZ5k-os4F_d9svImr_x8m",
-        analytics: "UA-118012943-1",
+        analytics: "G-PGGE3W1JGZ",
     },
 };
 
